87090750cad0eaf91d0520433d94c265d691e9f8
league <league@imac-12.attlocal.net>
2022-05-06 18:28:56 -0700

    working on TextUndoRedo

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -9,13 +9,44 @@ public class _01_IntroToStack {
     public static void main(String[] args) {
         // 1. Create a Stack of Doubles
         //    Don't forget to import the Stack class
+        Stack<Double> numbers = new Stack<Double>();
 
         // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+        Random r = new Random(); 
+        for(int i = 0; i<101; i++) {
+        	numbers.push(r.nextDouble()*100);
+        	
+        }
 
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+        String input0 = JOptionPane.showInputDialog("Enter one number between 0 and 100, inclusive");
+        String input1 = JOptionPane.showInputDialog("Enter another number between 0 and 100. inclusive");
+        
+        int num0 = Integer.parseInt(input0);
+        int num1 = Integer.parseInt(input1);
+        
+        if(num1 <= num0) {
+        	int temp = num0;
+        	num0 = num1; 
+        	num1 = temp;
+        }
+        
+        System.out.println("NUM 1: " + num0);
+        System.out.println("NUM 2: " + num1);
+        System.out.println();
 
         // 4. Pop all the elements off of the Stack. Every time a double is popped that is
         //    between the two numbers entered by the user, print it to the screen.
+        System.out.println("Popping elements off stack...");
+        System.out.println("Elements between " + num0 + " and " + num1 + " :");
+        Double num = 0.0;
+        
+        for(int i = 0; i<101; i++) {
+        	num = numbers.pop();
+        	if(num >= num0 && num <= num1) {
+        		System.out.println(num);
+        	}
+        }
 
 
         // EXAMPLE:
--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -1,8 +1,17 @@
 package _00_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener {
     /* 
      * Create a JFrame with a JPanel and a JLabel.
+     * 
      * 
      * Every time a key is pressed, add that character to the JLabel. It should
      * look like a basic text editor.
@@ -16,6 +25,51 @@ public class _02_TextUndoRedo {
      * pressed, the top Character is popped  off the Stack and added back to
      * the JLabel.
      */
+	
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	Stack<String> text;
+	public static void main(String[] args) {
+
+		
+	}
+	
+	void setup() {
+		frame = new JFrame();
+		panel = new JPanel();
+		label = new JLabel();
+		text = new Stack<String>();
+		
+		frame.add(panel);
+		
+		panel.add(label);
+		panel.addKeyListener(this);
+		
+		//label.setText(text);
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		//text = e.getKeyChar() + text; 
+		//label.setText(text);
+		//frame.pack();
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 
 }
